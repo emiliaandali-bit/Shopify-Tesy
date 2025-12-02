61f70c6f84093707ecb29e2242f06b465f8f4e46
emiliaandali-bit <emiliaandali@gmail.com>
2025-12-02 20:18:05 +0100

    Add Jest globals to TypeScript shims

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -9,3 +9,13 @@ declare module "crypto" {
 declare function describe(name: string, fn: () => void): void;
 declare function it(name: string, fn: () => void): void;
 declare function expect(actual: any): any;
+
+declare function beforeEach(fn: () => void): void;
+
+declare const jest: {
+  fn: (...args: any[]) => any;
+  spyOn: (...args: any[]) => any;
+  resetAllMocks: () => void;
+  clearAllMocks: () => void;
+  [key: string]: any;
+};
